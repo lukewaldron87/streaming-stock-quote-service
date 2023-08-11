4aec8d9d4477e28e35a72a32d2b4efcca0283ec9
Luke Waldron <lukewaldron87@gmail.com>
2023-08-11 11:03:58 +0200

    initial commit

---
+++ src/main/java/com/waldron/springframework/streamingstockquoteservice/StreamingStockQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.waldron.springframework.streamingstockquoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingStockQuoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingStockQuoteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waldron/springframework/streamingstockquoteservice/StreamingStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waldron.springframework.streamingstockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingStockQuoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

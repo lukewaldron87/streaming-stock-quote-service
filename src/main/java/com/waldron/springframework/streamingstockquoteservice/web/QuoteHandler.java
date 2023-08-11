fd79b0bdb9be80f7709cfa6eec2c19aa5765438a
Luke Waldron <lukewaldron87@gmail.com>
2023-08-11 11:52:28 +0200

    added QuoteHandler

---
+++ src/main/java/com/waldron/springframework/streamingstockquoteservice/web/QuoteHandler.java
@@ -0,0 +1,38 @@
+package com.waldron.springframework.streamingstockquoteservice.web;
+
+import com.waldron.springframework.streamingstockquoteservice.model.Quote;
+import com.waldron.springframework.streamingstockquoteservice.service.QuoteGeneratorService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_NDJSON;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class QuoteHandler {
+
+    private final QuoteGeneratorService quoteGeneratorService;
+
+    public Mono<ServerResponse> fetchQuotes(ServerRequest request){
+        int size = Integer.parseInt(request.queryParam("size").orElse("10"));
+        return ok().contentType(APPLICATION_JSON)
+                .body(
+                        quoteGeneratorService.fetchQuoteStream(Duration.ofMillis(100)).take(size),
+                        Quote.class
+                );
+    }
+
+    public Mono<ServerResponse> streamQuotes(ServerRequest request){
+        return ok().contentType(APPLICATION_NDJSON)
+                .body(quoteGeneratorService.fetchQuoteStream(Duration.ofMillis(100)), Quote.class);
+    }
+}
